89a6bc46096b52147ad5678318e92476b5832109
wuhuimin <wuhuimin236@pingan.com.cn>
2020-04-20 10:34:44 +0800

    1。nacos注册中心拿配置 2。目前主要实现 后台代码的测试，各个微服务dubbo之间调用正常。

---
+++ shanjupay-merchant/shanjupay-merchant-api/src/main/java/com/shanjupay/merchant/api/MerchantService.java
@@ -0,0 +1,39 @@
+package com.shanjupay.merchant.api;
+
+import com.shanjupay.common.domain.BusinessException;
+import com.shanjupay.merchant.api.dto.MerchantDTO;
+import com.shanjupay.merchant.api.dto.StaffDTO;
+import com.shanjupay.merchant.api.dto.StoreDTO;
+
+/**
+ * Created by Administrator.
+ */
+public interface MerchantService {
+
+    //根据 id查询商户
+     MerchantDTO queryMerchantById(Long id);
+
+    /**
+     *  注册商户服务接口，接收账号、密码、手机号，为了可扩展性使用merchantDto接收数据
+     * @param merchantDTO 商户注册信息
+     * @return 注册成功的商户信息
+     */
+    MerchantDTO createMerchant(MerchantDTO merchantDTO) throws BusinessException;
+
+    /**
+     * 资质申请接口
+     * @param merchantId 商户id
+     * @param merchantDTO 资质申请的信息
+     * @throws BusinessException
+     */
+    void applyMerchant(Long merchantId,MerchantDTO merchantDTO) throws BusinessException;
+
+
+    StoreDTO createStore(StoreDTO storeDTO) throws BusinessException;
+
+    StaffDTO createStaff(StaffDTO staffDTO) throws BusinessException;
+
+   void bindStaffToStore(Long storeId, Long staffId) throws BusinessException;
+
+
+}
